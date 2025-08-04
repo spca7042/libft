72aa37a30740a4538654bdca02cde769327cf589
spca7042 <sergiocalves@gmail.com>
2025-08-04 17:10:13 +0100

    Size (number of nodes) of list
    Counts the number of nodes in the list.

---
+++ ft_lstsize.c
@@ -0,0 +1,56 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstsize.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: seralves <seralves@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/18 18:59:00 by seralves          #+#    #+#             */
+/*   Updated: 2025/05/18 18:59:05 by seralves         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+/*
+Function name: ft_lstsize
+Prototype: int ft_lstsize(t_list *lst);
+Turn in files: -
+Parameters: lst: The beginning of the list.
+Return value: The length of the list
+External functs.: None
+Description: Counts the number of nodes in the list.
+*/
+
+#include "libft.h"
+
+int	ft_lstsize(t_list *lst)
+{
+	int	count;
+
+	count = 0;
+	while (lst)
+	{
+		count++;
+		lst = lst->next;
+	}
+	return (count);
+}
+/*
+#include <stdio.h>
+
+int main(void)
+{
+	char	*str = ft_strdup("nodo velho");
+	char	*strnew = ft_strdup("nodo novo");
+	t_list  *lista;
+	t_list	*novo_nodo;
+
+	lista = ft_lstnew(str);
+	novo_nodo = ft_lstnew(strnew);
+	ft_lstadd_front(&lista, novo_nodo);
+	printf("1 %s ; 2 %s\n", (char *)lista->content, 
+		(char *)lista->next->content);
+	printf("tamanho: %d", ft_lstsize(lista));
+	free (lista->next);
+	free (lista);
+	return (0);
+}*/
